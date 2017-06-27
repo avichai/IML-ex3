91c37007208e6a3fcad766eb5aadaaab5a0152f0
avichai <avichai.bendavid@mail.huji.ac.il>
2017-06-27 14:03:39 +0300

    Initial commit

---
+++ ex3.py
@@ -0,0 +1,340 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+import time
+
+
+def affine_layer_trunc(input, dim_out, act=tf.nn.relu):
+    input_shape = input.get_shape().as_list()
+    input_dim = input_shape[-1]
+    weights_shape = [input_dim, dim_out]
+
+    weights_init = tf.truncated_normal(weights_shape, stddev=0.1)
+    bias_init = tf.constant(0.1, shape=[dim_out])
+
+    weights = tf.Variable(weights_init)
+    biases = tf.Variable(bias_init)
+    preactivate = tf.matmul(input, weights) + biases
+
+    activations = act(preactivate, name='activation')
+    return activations
+
+
+def affine_layer_uniform(input, dim_out, act=tf.nn.relu):
+    input_shape = input.get_shape().as_list()
+    input_dim = input_shape[-1]
+    weights_shape = [input_dim, dim_out]
+
+    weights_init = tf.random_uniform(weights_shape)
+    bias_init = tf.constant(0.1, shape=[dim_out])
+
+    weights = tf.Variable(weights_init)
+    biases = tf.Variable(bias_init)
+    preactivate = tf.matmul(input, weights) + biases
+
+    activations = act(preactivate)
+    return activations
+
+
+def conv_layer_trunc(input, output_channels, ksize, stride, act=tf.nn.relu):
+    input_shape = input.get_shape().as_list()
+    input_channels = input_shape[-1]
+
+    initial_weight = tf.truncated_normal(
+        [ksize, ksize, input_channels, output_channels])
+    initial_biases = tf.zeros([output_channels])
+
+    weights = tf.Variable(initial_weight)
+    biases = tf.Variable(initial_biases)
+    c = tf.nn.conv2d(input, weights, strides=[1, stride, stride, 1],
+                     padding='SAME') + biases
+
+    return act(c)
+
+
+def conv_layer_uniform(input, output_channels, ksize, stride, act=tf.nn.relu):
+    input_shape = input.get_shape().as_list()
+    input_channels = input_shape[-1]
+
+    initial_weight = tf.random_uniform(
+        [ksize, ksize, input_channels, output_channels])
+    initial_biases = tf.ones([output_channels])
+
+    weights = tf.Variable(initial_weight)
+    biases = tf.Variable(initial_biases)
+    c = tf.nn.conv2d(input, weights, strides=[1, stride, stride, 1],
+                     padding='SAME') + biases
+
+    return act(c)
+
+
+def create_architucture_default(x, hidden_act=tf.nn.relu, initialize=True):
+    images = tf.reshape(x, [-1, 28, 28, 1])
+
+    if initialize:
+        conv1 = conv_layer_trunc(images, 32, ksize=5, stride=1, act=hidden_act)
+        pool1 = tf.nn.max_pool(conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1],
+                               padding='SAME')
+        conv2 = conv_layer_trunc(pool1, 64, ksize=3, stride=1, act=hidden_act)
+        pool2 = tf.nn.max_pool(conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1],
+                               padding='SAME')
+
+    else:
+        conv1 = conv_layer_uniform(images, 32, ksize=5, stride=1,
+                                   act=hidden_act)
+        pool1 = tf.nn.max_pool(conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1],
+                               padding='SAME')
+        conv2 = conv_layer_uniform(pool1, 64, ksize=3, stride=1, act=hidden_act)
+        pool2 = tf.nn.max_pool(conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1],
+                               padding='SAME')
+
+    shape = pool2.get_shape().as_list()
+    shape = [-1, np.product(shape[1:])]
+    flat = tf.reshape(pool2, shape)
+    aff = affine_layer_trunc(flat, 1024, act=hidden_act)
+    predict = affine_layer_trunc(aff, 10, act=tf.identity)
+    return predict
+
+
+def create_architucture_diff_arch(x, hidden_act=tf.nn.relu):
+    images = tf.reshape(x, [-1, 28, 28, 1])
+
+    conv1 = conv_layer_trunc(images, 16, ksize=3, stride=1, act=hidden_act)
+    pool1 = tf.nn.max_pool(conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1],
+                           padding='SAME')
+    conv11 = conv_layer_trunc(pool1, 32, ksize=3, stride=1, act=hidden_act)
+    pool11 = tf.nn.max_pool(conv11, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1],
+                            padding='SAME')
+    conv2 = conv_layer_trunc(pool11, 64, ksize=3, stride=1, act=hidden_act)
+    pool2 = tf.nn.max_pool(conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1],
+                           padding='SAME')
+
+    shape = pool2.get_shape().as_list()
+    shape = [-1, np.product(shape[1:])]
+    flat = tf.reshape(pool2, shape)
+    aff = affine_layer_trunc(flat, 1024, act=hidden_act)
+    predict = affine_layer_trunc(aff, 10, act=tf.identity)
+    return predict
+
+
+def test_batch_size():
+    x = tf.placeholder(tf.float32, [None, 784], name='x-input')
+    y = tf.placeholder(tf.float32, [None, 10], name='y-input')
+
+    eta = 0.001
+    prediction = create_architucture_default(x)
+    diff = tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=prediction)
+    cross_entropy = tf.reduce_mean(diff)
+    train_step = tf.train.GradientDescentOptimizer(eta).minimize(cross_entropy)
+    correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(prediction, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+    batch_options = [30, 300]
+    all_train_errors = []
+    all_test_errors = []
+    for s in batch_options:
+        start = time.time()
+        test, train = train_net(s, train_step, accuracy, x, y)
+        duration = time.time() - start
+        all_train_errors.append(train)
+        all_test_errors.append(test)
+        print("time took for batch size ", str(s), " is:", str(duration))
+
+    plot_errors(all_test_errors, all_train_errors, "batch size", batch_options)
+
+
+def test_learning_rates():
+    x = tf.placeholder(tf.float32, [None, 784], name='x-input')
+    y = tf.placeholder(tf.float32, [None, 10], name='y-input')
+
+    batch_size = 100
+    prediction = create_architucture_default(x)
+    diff = tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=prediction)
+    cross_entropy = tf.reduce_mean(diff)
+    correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(prediction, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+    eta_options = [1, 0.1, 0.01, 0.001]
+    all_train_errors = []
+    all_test_errors = []
+    for learn_rate in eta_options:
+        print('start: %s' % str(learn_rate))
+        train_step = tf.train.GradientDescentOptimizer(learn_rate).minimize(
+            cross_entropy)
+        test, train = train_net(batch_size, train_step, accuracy, x, y)
+        all_train_errors.append(train)
+        all_test_errors.append(test)
+
+    plot_errors(all_test_errors, all_train_errors, "learning rate", eta_options)
+
+
+def test_learning_algorithms():
+    x = tf.placeholder(tf.float32, [None, 784], name='x-input')
+    y = tf.placeholder(tf.float32, [None, 10], name='y-input')
+
+    batch_size = 100
+    eta = 0.001
+    prediction = create_architucture_default(x)
+    diff = tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=prediction)
+    cross_entropy = tf.reduce_mean(diff)
+    correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(prediction, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+    train_options = [tf.train.AdamOptimizer(eta).minimize(cross_entropy),
+                     tf.train.GradientDescentOptimizer(eta).minimize(cross_entropy)]
+    all_train_errors = []
+    all_test_errors = []
+    for train_step in train_options:
+        test, train = train_net(batch_size, train_step, accuracy, x, y)
+        all_train_errors.append(train)
+        all_test_errors.append(test)
+
+    plot_errors(all_test_errors, all_train_errors, "algorithm",
+                ["AdamOptimizer", "GradientDescentOptimizer"])
+
+
+def test_activation():
+    x = tf.placeholder(tf.float32, [None, 784], name='x-input')
+    y = tf.placeholder(tf.float32, [None, 10], name='y-input')
+
+    batch_size = 100
+    eta = 0.001
+
+    all_train_errors = []
+    all_test_errors = []
+    hidden_activation_functions = [tf.nn.tanh, tf.nn.relu]
+    for act in hidden_activation_functions:
+        prediction = create_architucture_default(x, act)
+        diff = tf.nn.softmax_cross_entropy_with_logits(labels=y,
+                                                       logits=prediction)
+        cross_entropy = tf.reduce_mean(diff)
+        train_step = tf.train.GradientDescentOptimizer(eta).minimize(
+            cross_entropy)
+        correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(prediction, 1))
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+        test, train = train_net(batch_size, train_step, accuracy, x, y)
+        all_train_errors.append(train)
+        all_test_errors.append(test)
+
+    plot_errors(all_test_errors, all_train_errors, "activation",
+                ["TanH", "ReLU"])
+
+
+def test_initialization():
+    x = tf.placeholder(tf.float32, [None, 784], name='x-input')
+    y = tf.placeholder(tf.float32, [None, 10], name='y-input')
+
+    batch_size = 100
+    eta = 0.001
+
+    all_train_errors = []
+    all_test_errors = []
+    initialization_options = [True, False]
+    act = tf.nn.relu
+    for opt in initialization_options:
+        prediction = create_architucture_default(x, act, opt)
+        diff = tf.nn.softmax_cross_entropy_with_logits(labels=y,
+                                                       logits=prediction)
+        cross_entropy = tf.reduce_mean(diff)
+        train_step = tf.train.GradientDescentOptimizer(eta).minimize(
+            cross_entropy)
+        correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(prediction, 1))
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+        test, train = train_net(batch_size, train_step, accuracy, x, y)
+        all_train_errors.append(train)
+        all_test_errors.append(test)
+
+    plot_errors(all_test_errors, all_train_errors, "initialization",
+                ["truncated_normal", "random_uniform"])
+
+
+def test_architecture():
+    x = tf.placeholder(tf.float32, [None, 784], name='x-input')
+    y = tf.placeholder(tf.float32, [None, 10], name='y-input')
+
+    batch_size = 100
+    eta = 0.001
+
+    all_train_errors = []
+    all_test_errors = []
+    architecture_options = [create_architucture_default,
+                            create_architucture_diff_arch]
+    act = tf.nn.relu
+
+    for opt in architecture_options:
+        prediction = opt(x, act)
+        diff = tf.nn.softmax_cross_entropy_with_logits(labels=y,
+                                                       logits=prediction)
+        cross_entropy = tf.reduce_mean(diff)
+        train_step = tf.train.GradientDescentOptimizer(eta).minimize(
+            cross_entropy)
+        correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(prediction, 1))
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+        test, train = train_net(batch_size, train_step, accuracy, x, y)
+        all_train_errors.append(train)
+        all_test_errors.append(test)
+
+    plot_errors(all_test_errors, all_train_errors, "architecture",
+                ["2 conv", "3 conv"])
+
+
+def plot_errors(test_errors, train_errors, param_desc, param_values):
+    x_axis = np.arange(0, len(test_errors[0]))
+
+    for i in range(len(param_values)):
+        cur_parm = str(param_values[i])
+        plt.plot(x_axis, test_errors[i], label=str(
+            'test with %s %s' % (param_desc, cur_parm)))
+        plt.plot(x_axis, train_errors[i], label=str(
+            'train with %s %s' % (param_desc, cur_parm)))
+
+    plt.title("detection, testing the change using different %s" % param_desc)
+
+    plt.ylabel("detection")
+    plt.xlabel("iterations")
+
+    plt.legend(loc='lower right')
+
+    plt.savefig(str("success_rates_%s.png" % param_desc), format='png')
+    plt.show()
+
+
+def train_net(batch_size, train_step, accuracy, x, y):
+    DEFAULT_ITERATION = 200
+    DEFAULT_TEST_FREQ = 10
+
+    sess = tf.InteractiveSession()
+    tf.global_variables_initializer().run()
+    acc_list = []
+
+    train_error = []
+    test_error = []
+
+    for i in range(DEFAULT_ITERATION):
+        xs, ys = mnist.train.next_batch(batch_size)
+        ys = ys.astype(np.float32)
+        acc, _ = sess.run([accuracy, train_step], feed_dict={x: xs, y: ys})
+        acc_list.append(acc)
+
+        if i % DEFAULT_TEST_FREQ == 0:
+            test_acc = sess.run(accuracy, feed_dict={x: mnist.test.images,
+                                                     y: mnist.test.labels})
+            train_error.append(np.mean(acc_list))
+            test_error.append(test_acc)
+            acc_list = []
+
+    sess.close()
+
+    return test_error, train_error
+
+
+if __name__ == "__main__":
+    mnist = input_data.read_data_sets('../MNIST_data/', one_hot=True)
+
+    test_batch_size()
+    test_learning_rates()
+    test_learning_algorithms()
+    test_activation()
+    test_initialization()
+    test_architecture()
---
+++ mnist_slim.py
@@ -0,0 +1,157 @@
+#!/usr/bin/env python
+# An MNIST interactive demo
+# Yoni Wexler. April 20, 2017
+
+from tensorflow.examples.tutorials.mnist import input_data
+import tensorflow as tf
+from tensorflow.contrib import slim
+import os
+import shutil
+import numpy as np
+import matplotlib.pyplot as plt
+# import cv2
+
+
+def build_network(images):
+    with tf.name_scope('LeNet'):
+        net = tf.reshape(images, [-1, 28, 28, 1])
+        net = slim.conv2d(net, 16, [5, 5], scope='conv1')
+        net = slim.max_pool2d(net, [2, 2], 2, scope='pool1')
+        net = slim.conv2d(net, 32, [5, 5], scope='conv2')
+        net = slim.max_pool2d(net, [2, 2], 2, scope='pool2')
+        net = slim.flatten(net)
+        net = slim.fully_connected(net, 128, scope='fc3')
+        embeddings = net    # So we can see the outputs in tensorboard
+        predictions = slim.fully_connected(net, 10, activation_fn=None,
+                                           scope='fc4')
+    return predictions, embeddings
+
+
+def train_net(basedir):
+    print('basedir = ', basedir)
+    mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+    shutil.rmtree(basedir, ignore_errors=True)
+    os.mkdir(basedir)
+
+    # The training data:
+    x = tf.placeholder(tf.float32, shape=[None, 784])
+    y = tf.placeholder(tf.float32, shape=[None, 10])
+
+    # Build the network, based on LeNet architecture:
+    predictions, embeddings = build_network(x)
+
+    # Define our loss function:
+    compute_loss = tf.reduce_mean(
+        tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=predictions))
+    compute_accuracy = tf.reduce_mean(
+        tf.cast(tf.equal(tf.argmax(predictions, axis=1), tf.argmax(y, axis=1)),
+                tf.float32))
+    # Define the optimizer:
+    train_step = tf.train.GradientDescentOptimizer(learning_rate=0.1).minimize(compute_loss)
+
+    # Show progress with TensorBoard:
+    tf.summary.scalar('Loss', compute_loss)
+    tf.summary.scalar('Accuracy', compute_accuracy)
+    summaries = tf.summary.merge_all()
+
+    # Now start optimizing:
+    with tf.Session() as sess:
+        # Optimize:
+        sess.run(tf.global_variables_initializer())
+
+        train_writer = tf.summary.FileWriter(basedir + '/train', sess.graph)
+        test_writer = tf.summary.FileWriter(basedir + '/test', sess.graph)
+
+        losses = []
+        accuracies = []
+        print("%10s | %10s | %5s%% | %5s%%" % ('Samples', 'Loss', 'Train', 'Test'))
+        for i in range(1, 500):
+            batchx, batchy = mnist.train.next_batch(32)            
+            _, loss, acc, summ = sess.run([train_step, compute_loss,
+                                           compute_accuracy, summaries],
+                                          feed_dict={x: batchx, y: batchy})
+            losses.append(loss)
+            accuracies.append(acc)
+            train_writer.add_summary(summ, i)
+
+            if i % 20 == 0:
+                tloss, tacc, summ = sess.run([compute_loss, compute_accuracy, summaries],
+                                             feed_dict={x: mnist.test.images,
+                                                        y: mnist.test.labels})
+                print("%10d | %10.6f | %5.2f%% | %5.2f%%" % (i*len(batchy), np.mean(losses),
+                                                            100.0 * np.mean(accuracies),
+                                                            100.0 * tacc))
+                losses = []
+                accuracies = []
+                test_writer.add_summary(summ, i)
+
+        # Save the resulting model:
+        saver = tf.train.Saver(max_to_keep=1)
+        saver.save(sess, basedir + '/mnist_checkpoint', global_step=i)
+
+
+def train_or_load_model(basedir):
+    latest_checkpoint = tf.train.latest_checkpoint(basedir)
+
+    if not latest_checkpoint:
+        train_net(basedir)
+        latest_checkpoint = tf.train.latest_checkpoint(basedir)
+        print('Now re-run to play with the model')
+        exit()
+        
+
+    sess = tf.Session()
+
+    x = tf.placeholder(tf.float32, shape=[None, 784])
+    # Build the network, based on LeNet architecture:
+    predictions, embeddings = build_network(x)
+
+    saver = tf.train.Saver()
+    saver.restore(sess, latest_checkpoint)
+
+    def evaluate_model(images):
+        return sess.run(predictions, feed_dict={x: images})
+
+    return evaluate_model
+
+
+def show_images_and_labels(images, labels):
+    plt.figure()
+    plt.clf()
+    n = images.shape[0]
+
+    ncols = int(np.ceil(np.sqrt(n)))
+    nrows = int(np.ceil(float(n) / ncols))
+
+    for i in range(n):
+        plt.subplot(nrows, ncols, i + 1)
+        plt.imshow(images[i].reshape([28, 28]), cmap='gray')
+        plt.axis('off')
+        plt.text(0, 27, str(labels[i]), color=(0.2, 1.0, 0.2))
+
+    plt.show()
+
+
+def examine_model(model):
+    # show test-set errors:
+    mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+    test_predictions = model(mnist.test.images)
+    test_predictions = np.argmax(test_predictions, axis=1)
+    acc = test_predictions == np.argmax(mnist.test.labels, axis=1)
+    mean_acc = np.mean(acc)
+    print("Test accuracy is: %f%%" % (100 * mean_acc))
+    mistakes = np.where(~acc)[0]
+    print('Found %d mistakes' % len(mistakes))
+    mistakes = mistakes[:100]   # Keep max 100
+    show_images_and_labels(mnist.test.images[mistakes], test_predictions[mistakes])
+
+
+if __name__ == "__main__":
+
+    basedir = 'mnist_models'
+
+    model = train_or_load_model(basedir)
+
+    examine_model(model)
+
+    # interactive_demo(model)
